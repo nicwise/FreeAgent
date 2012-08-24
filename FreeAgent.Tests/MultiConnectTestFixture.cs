006eed8e2ffeea89dec48f30b506b3e78c5a9a90
nicwise <nicw@fastchicken.co.nz>
2012-08-24 12:15:08 +0100

    Added in a test using 2 classes, to ttest the proxying

---
+++ FreeAgent.Tests/MultiConnectTestFixture.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NUnit.Framework;
+
+namespace FreeAgent.Tests
+{
+
+    public class MultiConnectTestFixture
+    {
+        protected FreeAgentClient Client, LiveClient;
+        protected AccessToken Token;
+
+        public virtual void Configure()
+        {
+
+        }
+
+        [SetUp]
+        public virtual void SetupClient()
+        {
+            Configure();
+
+           
+        }
+
+
+        [Test]
+        public void CanGetList()
+        {
+
+
+            FreeAgentClient.UseSandbox = true;
+            FreeAgentClient.Proxy = new WebProxy("127.0.0.1", 8888);
+
+            Client = new FreeAgentClient(KeyStorage.AppKey, KeyStorage.AppSecret);
+
+            var sandbox_bttest_token = new AccessToken
+            {
+                access_token = "",
+                refresh_token = KeyStorage.RefreshToken,
+                token_type = "bearer"
+            };
+
+            Client.CurrentAccessToken = sandbox_bttest_token;
+
+            try
+            {
+                var co = Client.Company.Single();
+            } catch
+            {
+                
+            }
+
+
+            FreeAgentClient.UseSandbox = false;
+            LiveClient = new FreeAgentClient(KeyStorage.AppKey, KeyStorage.AppSecret);
+
+
+
+            LiveClient.CurrentAccessToken = sandbox_bttest_token;
+
+            try
+            {
+                var co = LiveClient.Company.Single();
+            } catch
+            {
+                
+            }
+
+
+
+            
+            Assert.IsTrue(true);
+
+        }
+
+        
+    }
+}
