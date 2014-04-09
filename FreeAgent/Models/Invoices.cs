c193ede4685ee23e72d82ef4066337c701fa9728
Nic Wise <nicw@fastchicken.co.nz>
2014-04-10 09:41:27 +1200

    Added due_value and paid_value to invoices

--- FreeAgent/Models/Invoices.cs
+++ FreeAgent/Models/Invoices.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 
 namespace FreeAgent
 {
-	
 	//GET https://api.freeagent.com/v2/contacts
-	
 	//https://api.freeagent.com/v2/contacts
-	
 	public class Invoice : UpdatableModel, IRemoveUrlOnSerialization, IRemoveRecurringOnSerialization
 	{
 		public Invoice() : base()
@@ -15,39 +12,64 @@ namespace FreeAgent
 			invoice_items = new List<InvoiceItem>();
 		}
 
-		
 		public string reference { get; set; }
+
 		public string contact { get; set; }
+
 		public string project { get; set; }
+
 		public string status { get; set; }
-		
+
 		public double discount_percent { get; set; }
+
 		public string dated_on { get; set; }
+
 		public string due_on { get; set; }
+
 		public double exchange_rate { get; set; }
+
 		public int payment_terms_in_days { get; set; }
+
 		public string currency { get; set; }
+
 		public string ec_status { get; set; }
+
 		public string written_off_date { get; set; }
-        public double net_value { get; set; }
-        public double sales_tax_value {get; set; }
-        public string comments { get; set; }
+
+		public double net_value { get; set; }
+
+		public double sales_tax_value { get; set; }
+
+		public double due_value { get; set; }
+
+		public double paid_value { get; set; }
+
+		public string comments { get; set; }
+
 		public List<InvoiceItem> invoice_items { get; set; }
 	}
-	
+
 	public class InvoiceItem
 	{
 		public string url { get; set; }
+
 		public int position { get; set; }
+
 		public string item_type  { get; set; }
+
 		public double quantity { get; set; }
+
 		public double price { get; set; }
+
 		public string description { get; set; }
+
 		public double sales_tax_rate { get; set; }
+
 		public double second_sales_tax_rate { get; set; }
+
 		public string category { get; set; }
 	}
-	
+
 	public class InvoiceStatus
 	{
 		public static string Draft = "Draft";
@@ -55,14 +77,14 @@ namespace FreeAgent
 		public static string Sent = "Sent";
 		public static string Cancelled = "Cancelled";
 	}
-	
+
 	public class InvoiceECStatus
 	{
 		public static string NonEc = "Non-Ec";
 		public static string ECGoods = "EC Goods";
 		public static string ECServices = "EC Services";
 	}
-	
+
 	public class InvoiceItemType
 	{
 		public static string Hours = "Hours";
@@ -80,24 +102,24 @@ namespace FreeAgent
 		public static string Credit = "Credit";
 		public static string VAT = "VAT";
 		public static string NoUnit = "No Unit";
-
 	}
 
-    public class InvoiceViewFilter 
-    {
-        public static string RecentOpenOrOverdue = "recent_open_or_overdue";
-        public static string OpenOrOverdue = "open_or_overdue";
-        public static string Draft = "draft";
-        public static string ScheduledToEmail = "scheduled_to_email";
-        public static string ThankYouEmails = "thank_you_emails";
-        public static string ReminderEmails = "reminder_emails";
-        private static string lastNMonths = "last_{0}_months";
-        public static string LastNMonths(int months = 1)
-        {
-            return string.Format(lastNMonths, months);
-        }
-    }
-	
+	public class InvoiceViewFilter
+	{
+		public static string RecentOpenOrOverdue = "recent_open_or_overdue";
+		public static string OpenOrOverdue = "open_or_overdue";
+		public static string Draft = "draft";
+		public static string ScheduledToEmail = "scheduled_to_email";
+		public static string ThankYouEmails = "thank_you_emails";
+		public static string ReminderEmails = "reminder_emails";
+		private static string lastNMonths = "last_{0}_months";
+
+		public static string LastNMonths(int months = 1)
+		{
+			return string.Format(lastNMonths, months);
+		}
+	}
+
 	public class InvoiceWrapper
 	{
 		public InvoiceWrapper()
@@ -105,43 +127,51 @@ namespace FreeAgent
 			
 			invoice = null;
 		}
+
 		public Invoice invoice { get; set; }
-		
 	}
-	
-    public class InvoicesWrapper
-    {
-        public InvoicesWrapper()
-        {
-            invoices = new List<Invoice>();
 
-        }
+	public class InvoicesWrapper
+	{
+		public InvoicesWrapper()
+		{
+			invoices = new List<Invoice>();
 
-        public List<Invoice> invoices { get; set; }
-    }
+		}
+
+		public List<Invoice> invoices { get; set; }
+	}
 
 	public class InvoiceEmailWrapper
 	{
-		public InvoiceEmailWrapper() { invoice = new InvoiceEmail (); }
+		public InvoiceEmailWrapper()
+		{
+			invoice = new InvoiceEmail();
+		}
+
 		public InvoiceEmail invoice { get; set; }
 	}
 
 	public class InvoiceEmail
 	{
-		public InvoiceEmail() { email = new InvoiceEmailDetail (); }
+		public InvoiceEmail()
+		{
+			email = new InvoiceEmailDetail();
+		}
+
 		public InvoiceEmailDetail email { get; set; }
 	}
 
-	public class InvoiceEmailDetail 
+	public class InvoiceEmailDetail
 	{
 		public string to { get; set; }
+
 		public string @from { get; set; }
+
 		public string subject { get; set; }
+
 		public string body { get; set; }
 	}
-
-	
-	
 }
 
 
