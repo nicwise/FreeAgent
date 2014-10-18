949e02bd715b44113f4adc004d48a0b8fca0d2c9
daveparker <dave@arrowflex.com>
2014-10-18 13:26:22 +0100

    remove  ds store, get bill fixture to compile

---
+++ FreeAgent.Tests/KeyStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FreeAgent.Tests
+{
+    public static class KeyStorage
+    {
+        public static bool UseSandbox { get { return true; } }
+        public static bool UseProxy { get { return true; } }
+        public static string AppKey { get { return "*your appkey here*"; } }
+        public static string AppSecret { get { return "*your appsecret here*"; } }
+        public static string RefreshToken { get { return "*your refreshtoken here*"; } }
+    }
+}
